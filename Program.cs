5e47d423f2874cf92b1ac028708ba3a51ba5eb56
Victor <vovovkaba@gmail.com>
2023-11-01 02:14:51 -0700

    Added a rough draft of the command options

--- Program.cs
+++ Program.cs
@@ -16,12 +16,50 @@ Console.WriteLine("  | |1| |2| |3|  |x| | ");
 Console.WriteLine("  | |.| |0| |=|  |/| | ");
 Console.WriteLine("  |__________________| ");
 Console.WriteLine(" ");
-Console.WriteLine("**Which operation would you like to do??");
+Console.WriteLine("Which operation would you like to do??");
 Console.WriteLine("A - Addition");
 Console.WriteLine("S - Subtraction");
 Console.WriteLine("M - Multiplication");
 Console.WriteLine("D - Division");
 Console.WriteLine("Q - To quit");
+try
+{
+   char option = Console.ReadKey(true).KeyChar;
+}
+catch(Exception e)
+{ 
+  Console.WriteLine("Please enter a valid command");
+}
+
+int Operate(char o)
+{
+    int x = 0;
+    int y = 0;
+    if (o == 'Q') 
+    {
+        Environment.Exit(0);
+    
+    }
+    switch (o)
+    {
+        case 'A':
+            return x + y;
+        case 'S':
+            return x - y;
+        case 'M':
+            return x * y;
+        case 'D':
+            return x / y;
+        default:
+            return 0;
+    }
+    Console.Write("Enter your first number: ");
+    x = int.Parse(Console.ReadLine());
+    Console.Write("Enter your second number:");
+    y = int.Parse(Console.ReadLine());
 
 
+
+
+}
 
