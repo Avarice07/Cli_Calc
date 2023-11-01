d8475a41030723f2664a2295bd6d4a83a29f278a
Victor <vovovkaba@gmail.com>
2023-11-01 01:40:26 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.BackgroundColor = ConsoleColor.Magenta;
+//Console.BackgroundColor = ConsoleColor.Blue;
+
+Console.WriteLine("TIME TO CALCULATE");
+Console.WriteLine(" ");
+Console.WriteLine("  ____________________ ");
+Console.WriteLine("  |      SEIKO       | ");
+Console.WriteLine("  |  _____________   | ");
+Console.WriteLine("  | |  1 + 1 = 11 |  | ");
+Console.WriteLine("  |  -------------   | ");
+Console.WriteLine("  |                  | ");
+Console.WriteLine("  | |7| |8| |9|  |+| | ");
+Console.WriteLine("  | |4| |5| |6|  |-| | ");
+Console.WriteLine("  | |1| |2| |3|  |x| | ");
+Console.WriteLine("  | |.| |0| |=|  |/| | ");
+Console.WriteLine("  |__________________| ");
+Console.WriteLine(" ");
+Console.WriteLine("**Which operation would you like to do??");
+Console.WriteLine("A - Addition");
+Console.WriteLine("S - Subtraction");
+Console.WriteLine("M - Multiplication");
+Console.WriteLine("D - Division");
+Console.WriteLine("Q - To quit");
+
+
+
